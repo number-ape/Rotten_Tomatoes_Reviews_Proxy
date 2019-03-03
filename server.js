b5ff21e19a2062f71ead6ea2aa4373fbc3c4a029
Bernard <laubernardsy@gmail.com>
2019-03-02 21:13:16 -0500

    Changed logo, css and server routes

--- server.js
+++ server.js
@@ -3,9 +3,7 @@ const app = express();
 const port = process.env.PORT || 9000;
 
 app.use(express.static('public'));
-// app.all('*', (req, res, next) => {
-//   res.sendFile(__dirname + '/public/index.html');
-// });
+
 app.get('/', function (req, res, next) {
   res.sendFile(__dirname + '/public/index.html');
 });
@@ -15,6 +13,10 @@ app.get('/m/', function (req, res, next) {
 });
 
 app.get('/m/:title_url/', function (req, res, next) {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get('/m/:title_url/:id', function (req, res, next) {
   res.sendFile(__dirname + '/public/index.html');
 });
 
